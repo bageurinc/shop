6cd6e407dc4694edd64e75bde277f0a38080c7ad
ariqamuharam <ariqamuharam@gmail.com>
2020-11-26 19:14:43 +0700

    kategori

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,7 @@ Route::name('bageur.')->group(function () {
 	Route::group(['prefix' => 'bageur/v1'], function () {
 		Route::apiResource('produk', 'bageur\ecommerce\ProdukCmsController');
 		Route::apiResource('order', 'bageur\ecommerce\OrderCmsController');
+		Route::apiResource('kategori', 'bageur\ecommerce\KategoriController');
 		Route::apiResource('comment', 'bageur\ecommerce\CommentController');
 		Route::apiResource('review', 'bageur\ecommerce\ReviewController');
 		Route::apiResource('cart', 'bageur\ecommerce\CartController');
